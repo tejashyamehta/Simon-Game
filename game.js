f08552541532156afd9fbc267a2a40ed43423477
tejashyamehta <tejashya@gmail.com>
2025-06-11 13:11:31 +0530

    Sound functionality and button animation added

--- game.js
+++ game.js
@@ -1,12 +1,21 @@
 var buttonColours = ["red","blue","green","yellow"];
 gamePattern = []
 
+var sounds = {
+ blue:new Audio("sounds/blue.mp3"),
+ green:new Audio("sounds/green.mp3"),
+ red:new Audio("sounds/red.mp3"),
+ wrong:new Audio("sounds/wrong.mp3"),
+ yellow:new Audio("sounds/yellow.mp3")
+}
+
 
 
 function nextSequence(){
-    randomNumber = Math.round(Math.random()*3);
+    randomNumber = Math.floor(Math.random()*4);
     randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $("."+randomChosenColour).fadeOut(100).fadeIn(100);
+    sounds[randomChosenColour].play();
 }
